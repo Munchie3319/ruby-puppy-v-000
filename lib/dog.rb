46e0bf75e8b8d011984844cc4a3826e91e461e78
rosa lee <rosalee0116@gmail.com>
2020-04-05 21:29:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,29 +1,12 @@
 # Add your code here
 class Dog
-  attr_reader :name
-  @@all = []
+  @all = []
+  attr_accessor :name
 
   def initialize(name)
     @name = name
-    save
-  end
-
-  def self.all
-    @@all
-  end
-
-  def self.clear_all
-    @all.clear
-  end
-
-  def self.print_all
-    @all.each do |x|
-      puts x.name
-    end
-  end
-
-  def save
     @all << self
   end
 
 end
+
